8bfc4738e69932ab7bed34ac3f2842b3b9689d52
mharnetiaux <mattskyh@gmail.com>
2019-02-01 18:40:14 -0800

    not perfect, but started working with multiple objects on a page.

--- app/keyboard/touch/Right.js
+++ app/keyboard/touch/Right.js
@@ -6,7 +6,7 @@ class Right extends Component{
         this.moveRight = this.moveRight.bind(this);
         this.stop = this.stop.bind(this);
         this.state = {
-            trans: 0,
+            trans: 2,
             property: 'transform'
         };
     }
@@ -16,15 +16,13 @@ class Right extends Component{
               wheel = document.getElementsByClassName('wheel'),
               keyCode = e.keyCode;
 
-        for(let i = 0; i < wheel.length; i++){
-            wheel[i].classList.add("spin");
-        }
-
-
         if(keyCode === 39) {
             this.setState({trans: this.state.trans += 2});
             tank.style[this.state.property] = 'translateX(' + this.state.trans + 'rem)';
             tank.classList.add('move-right');
+            Object.values(wheel).forEach((i) => {
+                i.classList.add("spin");
+            });
         }
     }
 
@@ -34,9 +32,9 @@ class Right extends Component{
 
         tank.classList.remove('move-right');
 
-        for(let i = 0; i < wheel.length; i++){
-            wheel[i].classList.remove("spin");
-        }
+        Object.values(wheel).forEach((i) => {
+            i.classList.remove("spin");
+        });
     }
 
     componentDidMount() {

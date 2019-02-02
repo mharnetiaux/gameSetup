fc74a1c42a73332ffa951b0c3a4e8a185abd0be4
mharnetiaux <mattskyh@gmail.com>
2019-02-01 17:30:18 -0800

    update to movement.

--- app/keyboard/touch/Right.js
+++ app/keyboard/touch/Right.js
@@ -4,6 +4,7 @@ class Right extends Component{
     constructor(){
         super();
         this.moveRight = this.moveRight.bind(this);
+        this.stop = this.stop.bind(this);
         this.state = {
             trans: 0,
             property: 'transform'
@@ -11,19 +12,36 @@ class Right extends Component{
     }
 
     moveRight(e) {
-        const character = document.querySelector('.tank'),
+        const tank = document.querySelector('.tank'),
+              wheel = document.getElementsByClassName('wheel'),
               keyCode = e.keyCode;
+
+        for(let i = 0; i < wheel.length; i++){
+            wheel[i].classList.add("spin");
+        }
 
 
         if(keyCode === 39) {
-            this.setState({trans: this.state.trans += 3});
-            character.style[this.state.property] = 'translateX(' + this.state.trans + 'rem)';
-            character.classList.add('move-right');
+            this.setState({trans: this.state.trans += 2});
+            tank.style[this.state.property] = 'translateX(' + this.state.trans + 'rem)';
+            tank.classList.add('move-right');
+        }
+    }
+
+    stop() {
+        const tank = document.querySelector('.tank'),
+              wheel = document.getElementsByClassName('wheel');
+
+        tank.classList.remove('move-right');
+
+        for(let i = 0; i < wheel.length; i++){
+            wheel[i].classList.remove("spin");
         }
     }
 
     componentDidMount() {
         document.addEventListener('keydown', this.moveRight, false);
+        document.addEventListener('keyup', this.stop, false);
     }
 
     render(){
